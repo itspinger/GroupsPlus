5c57d7c2d19f10a000549b298ec8bddc095485ca
Dimitrije Mijailovic <pinger.business@gmail.com>
2024-10-07 20:04:23 +0200

    feat: add helper class for messages.yml

---
+++ src/main/java/io/pinger/groups/config/ExternalConfigurationAdapter.java
@@ -0,0 +1,61 @@
+package io.pinger.groups.config;
+
+import io.pinger.groups.GroupsPlus;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Map;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+public abstract class ExternalConfigurationAdapter {
+    private final GroupsPlus plugin;
+    private final File file;
+
+    protected YamlConfiguration configuration;
+
+    public ExternalConfigurationAdapter(GroupsPlus plugin, String name)  {
+        this(plugin, name, false);
+    }
+
+    public ExternalConfigurationAdapter(GroupsPlus plugin, String name, boolean load) {
+        this.plugin = plugin;
+        this.file = new File(plugin.getDataFolder(), name);
+        this.configuration = YamlConfiguration.loadConfiguration(this.file);
+
+        // If the load is false
+        // We shouldn't replace if the file is not empty
+        if (!load && this.file.length() > 0) {
+            return;
+        }
+
+        // Load resource by this name
+        // And add the configuration section
+        final InputStream inputStream = plugin.getResource(name);
+        if (inputStream == null) {
+            throw new IllegalArgumentException("File resource cannot be found (" + name + ")");
+        }
+
+        // Try to read from the input stream
+        // And add to the file
+        try (final Reader reader = new InputStreamReader(inputStream)) {
+            final YamlConfiguration keyed = YamlConfiguration.loadConfiguration(reader);
+
+            // If we shouldn't copy the defaults, return here
+            // Copy the defaults
+            this.configuration.options().copyDefaults(true);
+
+            // Try to add defaults from resourceConfig
+            // To the main configuration, by looping over the keys
+            for (final Map.Entry<String, Object> entries : keyed.getValues(true).entrySet()) {
+                this.configuration.addDefault(entries.getKey(), entries.getValue());
+            }
+
+            this.configuration.save(this.file);
+        } catch (IOException e) {
+            plugin.getLogger().info("Failed to create a file with name " + name);
+        }
+    }
+
+}
---
+++ src/main/java/io/pinger/groups/config/MessageConfiguration.java
@@ -0,0 +1,46 @@
+package io.pinger.groups.config;
+
+import io.pinger.groups.GroupsPlus;
+import org.bukkit.ChatColor;
+
+public class MessageConfiguration extends ExternalConfigurationAdapter {
+
+    public MessageConfiguration(GroupsPlus plugin) {
+        super(plugin, "messages.yml", true);
+    }
+
+    public static String color(String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+
+    public boolean has(String key) {
+        return this.configuration.get(key) != null;
+    }
+
+    public String of(String key, boolean translate) {
+        String value = this.configuration.getString(key);
+
+        // Update value
+        value = value == null ? "" : value;
+
+        if (!translate)
+            return ChatColor.stripColor(value);
+
+        // Out this
+        return ChatColor.translateAlternateColorCodes('&', value);
+    }
+
+    public String of(String key) {
+        return this.of(key, true);
+    }
+
+    private String ofFormatted(String key, boolean translate, Object... objects) {
+        return String.format(this.of(key, translate), objects);
+    }
+
+    public String ofFormatted(String key, Object... objects) {
+        return this.ofFormatted(key, true, objects);
+    }
+
+
+}
