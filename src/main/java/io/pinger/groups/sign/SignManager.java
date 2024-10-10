69245272955657567f7a699dcfecc92c17c6609d
Dimitrije Mijailovic <pinger.business@gmail.com>
2024-10-10 21:55:06 +0200

    refactor: move sign processors to sign manager (hide them from others)

--- src/main/java/io/pinger/groups/sign/SignManager.java
+++ src/main/java/io/pinger/groups/sign/SignManager.java
@@ -1,7 +1,5 @@
 package io.pinger.groups.sign;
 
-import io.pinger.groups.sign.processors.PlayerGroupProcessor;
-import io.pinger.groups.sign.processors.PlayerNameProcessor;
 import io.pinger.groups.user.User;
 import io.pinger.groups.util.Text;
 import java.util.Arrays;
@@ -42,4 +40,39 @@ public class SignManager {
         return newLine;
     }
 
+    public interface SignProcessor {
+
+        boolean shouldReplaceLine(String line);
+
+        String replaceLine(User user, String line);
+
+    }
+
+    public static class PlayerNameProcessor implements SignProcessor {
+        private static final String NAME_PLACEHOLDER = "{name}";
+
+        @Override
+        public boolean shouldReplaceLine(String line) {
+            return line.contains(NAME_PLACEHOLDER);
+        }
+
+        @Override
+        public String replaceLine(User user, String line) {
+            return line.replace(NAME_PLACEHOLDER, user.toPlayer().getName());
+        }
+    }
+
+    public static class PlayerGroupProcessor implements SignProcessor {
+        private static final String RANK_PLACEHOLDER = "{rank}";
+
+        @Override
+        public boolean shouldReplaceLine(String line) {
+            return line.contains(RANK_PLACEHOLDER);
+        }
+
+        @Override
+        public String replaceLine(User user, String line) {
+            return line.replace(RANK_PLACEHOLDER, user.getHighestGroupPrefix("&e"));
+        }
+    }
 }
